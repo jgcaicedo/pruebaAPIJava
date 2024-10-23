3251cf90d14e68492860630e8bd62e980b7eacda
jgcaicedo <jgcaicedo202@gmail.com>
2024-10-22 19:09:28 -0500

    Revert "Subiendo mi backend a producción"
    This reverts commit f5aeaab8b69c85bae3ab2a78c059d90bff8b4b97.

--- src/main/java/com/api/Prueba/Producto/repository/ProductoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.api.Prueba.Producto.repository;
-
-
-import com.api.Prueba.Producto.model.Producto;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductoRepository extends JpaRepository<Producto, Integer> {}
